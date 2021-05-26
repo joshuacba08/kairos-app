4dd295dd8b15f1d044625f1abb067d77845bdf00
Josué Anderson <47667368+joshuacba08@users.noreply.github.com>
2021-05-25 21:12:26 -0300

    se agregan comentarios en ITemList

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -6,14 +6,17 @@ import './styles/ItemList.css'
 
 const ItemList = () => {
 
+    //usaré useState para luego en products introducir los productos que consuma de mi API o en este caso del array de productos que tengo en la carpeta data
     const [products, setProducts] = useState([]);
 
+    //esta función devuelve una promesa, en la cual hay un setTimeout con 2 segundos que devuelve mi array de productos.
     const getProducts = new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(productList);
         }, 2000);
     })
 
+    //esta función implemente un async que setea la variable products de mi useState y le ingresa mi array de productos
     const getProductsFromDB = async () => {
         try {
             const result = await getProducts;
@@ -23,16 +26,18 @@ const ItemList = () => {
         }
     }
 
+    //useEffect ejecutará la función getProductsFromDB simulando que estamos consumiendo los datos desde una base de datos.
+    //Esto se ejecutará solo una vez ya que el array de dependencias "[]" está vacío.
     useEffect(() => {
         getProductsFromDB();
     }, [])
 
     return (
         <div className="product-list-container">
-            {
-                products.length ?
+            { //estaré implementando un ternario (ver documentación de mozilla) en el cual pregunto por el número de elementos que hay en products
+                products.length ? //si en products no hay nada, el valor será "0" y en ese caso, un cero se puede interpretar como un valor booleano por lo cual se considerará como falso
                     <>
-                        {
+                        { //Luego de dos segundos, products tendrá los productos dentro, y por lo tanto el products.length será distinto de "0", así que se puede interpretar como un valor booleano "true"
                             products.map((product) => {
                                 return <div key={product.itemID}>
                                     <Item 
@@ -46,7 +51,7 @@ const ItemList = () => {
                         }
                     </>
                     :
-                    <p>Cargando productos...</p>
+                    <p>Cargando productos...</p>//Si products.length es "0" se interpreta como falso y por lo tanto se imprimirá "cargando productos"
             }
 
         </div>
